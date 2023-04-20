a71e17c71bdfd7ca7bc1cb4aa56a43d3ffc711a5
david terranova <david@smallpdf.com>
2023-04-20 12:27:21 +0200

    clean pg commented lines

--- pkg/pg/open.go
+++ pkg/pg/open.go
@@ -8,7 +8,6 @@ import (
 )
 
 type DBConfig struct {
-	// DSN      string `envconfig:"READ_CONN_STRING" required:"true"`
 	Schema   string `envconfig:"SCHEMA" default:"public"`
 	Username string `envconfig:"USERNAME" required:"true"`
 	Password string `envconfig:"PASSWORD" required:"true"`
@@ -19,7 +18,6 @@ type DBConfig struct {
 }
 
 func Open(cfg DBConfig) (*gorm.DB, error) {
-	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%d search_path=%s %s",
 		cfg.Host, cfg.Username, cfg.Password, cfg.Name, cfg.Port, cfg.Schema, cfg.Params,
